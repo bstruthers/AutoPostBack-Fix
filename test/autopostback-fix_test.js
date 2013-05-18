e27cc063ada9bb48184aadf22e477c9994e3a114
Ben Struthers <bstruthers@gmail.com>
2013-05-17 21:25:03 -0500

    Added basic unit tests

--- test/autopostback-fix_test.js
+++ test/autopostback-fix_test.js
@@ -20,11 +20,56 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery.autopostback-fix');
+  module('autopostback-fix');
 
-  test('empty', function() {
+  test('constructor test', function() {
     expect(1);
-    strictEqual($, $, 'should always pass');
+
+    var ddlList        = $('#ddlList')[0],
+        ddlListHandler = new bstruthers.AutoPostBackFix(ddlList);
+
+    strictEqual(typeof ddlListHandler, 'object', 'Instance of AutoPostBackFix constructed');
   });
 
+  test('onchange test', function() {
+    expect(3);
+
+    var $ddlList = $('#ddlList'),
+        onchange = $ddlList.attr('onchange'),
+        ddlListHandler;
+
+    // Ensure the onchange attribute is on our test element
+    notStrictEqual($ddlList.attr('onchange'), undefined, 'onchange attribute present');
+
+    // Create a new instance, this removes the onchange attribute
+    ddlListHandler = new bstruthers.AutoPostBackFix($ddlList[0]);
+    strictEqual($ddlList.attr('onchange'), undefined, 'onchange attribute removed');
+
+    // Calling destory will restore the original onchange attribute
+    ddlListHandler.destroy();
+    strictEqual($ddlList.attr('onchange'), onchange, 'onchange attribute restored to original state');
+  });
+
+  // Doesn't seem like there's a way to test this - I'm open to ideas
+  // test('handlers test', function() {
+  //   expect(2);
+
+  //   var ddlList        = $('#ddlList')[0],
+  //       ddlListHandler,
+  //       eventListeners = getEventListeners(ddlList)
+
+  //   // Ensure no handlers are already attached
+
+  //   // Create a new instance, this will automatically add the handlers
+  //   ddlListHandler = new bstruthers.AutoPostBackFix(ddlList);
+
+  //   // Calling disable will remove the handlers
+  //   ddlListHandler.disable();
+
+  //   // Calling enable will re-add the handlers
+  //   dlListHandler.enable();
+
+  //   // Calling destory will remove the handlers
+  //   ddlListHandler.destroy();
+  // });
 }(jQuery));
